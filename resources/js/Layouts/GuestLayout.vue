f1451b84de5f2823fc9798710c92c52f00d54b97
ukki <iu.cashirskaia@yandex.ru>
2025-03-05 11:51:30 +0300

    начало работы на sqlite

---
+++ resources/js/Layouts/GuestLayout.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+    <div
+        class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0"
+    >
+        <div
+            class="mt-6 w-full overflow-hidden bg-white p-10 shadow-md sm:max-w-md sm:rounded-3xl"
+        >
+            <slot />
+        </div>
+    </div>
+</template>
