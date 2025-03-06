80375241cc824ddf3e77d7516fe15f22c2c57aec
ukki <iu.cashirskaia@yandex.ru>
2025-03-06 12:44:55 +0300

    частично готовый календарь и кнопки

---
+++ resources/js/Components/datePicker.vue
@@ -0,0 +1,28 @@
+<template>
+    <VDatePicker v-model="date" color="indigo">
+        <template #footer>
+            <div class="flex w-full gap-2 px-3 pb-3">
+                <justButton
+                    color="gray"
+                    text="Отмена"
+                    @click="setToday"
+                ></justButton>
+                <button
+                    class="w-full rounded-md bg-indigo-600 px-3 py-1 font-bold text-white hover:bg-indigo-700"
+                    @click="setToday"
+                >
+                    Today
+                </button>
+            </div>
+        </template>
+    </VDatePicker>
+</template>
+<script setup>
+import { ref } from 'vue';
+import justButton from './justButton.vue';
+const date = ref(new Date());
+function setToday() {
+    date.value = new Date();
+}
+</script>
+<!-- https://vcalendar.io/?ref=madewithvuejs.com -->
---
+++ resources/js/Components/justButton.vue
@@ -0,0 +1,36 @@
+<script setup>
+const props = defineProps({
+    color: String,
+});
+let colorArray = [
+    [' bg-gray-200 hover:bg-gray-300 active:bg-gray-300 ', 'text-gray-800'],
+    [
+        ' bg-gray-800 hover:bg-gray-700 disabled:bg-gray-600 ',
+        'text-white disabled:text-gray-500',
+    ],
+    [' bg-gray-200 hover:bg-gray-300 active:bg-gray-300 ', 'text-white'],
+    [' bg-gray-200 hover:bg-gray-300 active:bg-gray-300 ', 'text-gray-800'],
+];
+let style;
+switch (props.color) {
+    case 'gray':
+        style = colorArray[0];
+        break;
+    case 'dark':
+        style = colorArray[1];
+        break;
+    case 'blue':
+        style = colorArray[2];
+        break;
+    case 'noColor':
+        style = colorArray[3];
+        break;
+    default:
+        break;
+}
+</script>
+<template>
+    <button :class="'rounded-lg px-5 py-2.5 text-white ' + style[0]">
+        <slot :class="'text-sm' + style[1]"></slot>
+    </button>
+</template>
