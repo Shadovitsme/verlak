2e63836931564ec2bb745bdbaf252c144439c3f0
ukki <iu.cashirskaia@yandex.ru>
2025-03-06 16:49:45 +0300

    заготовка карточчки информационной

---
+++ resources/js/Components/tableCard.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue';
+import TablePlaceholder from './tablePlaceholder.vue';
+
+const props = defineProps({
+    head: String,
+});
+
+let toggleTable = ref(false);
+</script>
+<template>
+    <!-- TODO понять откуда берется 1348 -->
+    <div class="h-fit w-[1348px] rounded-lg bg-white px-4 py-2">
+        <div class="flex justify-between">
+            <button @click="toggleTable = !toggleTable" class="flex px-5">
+                <p class="my-auto mr-2 text-sm text-gray-800">
+                    {{ props.head }}
+                </p>
+                <img
+                    class="size-5"
+                    src="/assets/icons/arrows/arrow-right-1.svg"
+                />
+            </button>
+            <img src="/assets/icons/system/delete.svg" />
+        </div>
+        <TablePlaceholder v-if="toggleTable"></TablePlaceholder>
+    </div>
+</template>
--- resources/js/Pages/testAdmin.vue
+++ resources/js/Pages/testAdmin.vue
@@ -1,8 +1,9 @@
 <template>
-        <Header></Header>
-
+    <Header></Header>
+    <TableCard head="test Head" class="mt-20"></TableCard>
 </template>
 
 <script setup>
 import Header from '@/Components/header.vue';
+import TableCard from '@/Components/tableCard.vue';
 </script>
